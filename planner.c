b6b490648476a278d3626b6f235bbb1a1c4a422b
Yash Seeta <yashseeta94@gmail.com>
2018-12-14 19:26:54 +0100

    Planner-Draft

--- planner.c
+++ planner.c
@@ -21,35 +21,89 @@
 #define PLANNER_POLL (10/portTICK_RATE_MS)
 int i = 0;
 PinEvent event;
+static portTickType xLastWakeTime;
 
+int stop_state, is_target_set = 0;
+int floor_arr[] = {0,0,0};
+
+void addFloor (int floor) {
+	// check if floor is already called
+	for( i = 0; i < 3; i++ ){
+		if(floor_arr[i] == floor) {
+			return;
+		}
+	}
+	//check special case for floor2
+	if(floor == 2){	
+		// check that elevator is not too close to floor 2 
+		// e.g.:  {3,1,0} -> {2,3,1}
+		if( (floor_arr[0] == 3 && getCarPosition() <= (FLOOR_1_POS + FLOOR_2_POS)/2) || 
+			(floor_arr[0] == 1 && getCarPosition() >= ((FLOOR_3_POS + FLOOR_2_POS)/2)) ) {
+			// add floor 2 as first target 
+			floor_arr[2] = floor_arr[1];
+			floor_arr[1] = floor_arr[0];
+			floor_arr[0] = floor;
+			is_target_set = 0;
+			return;
+		} else {
+			// else add floor 2 as second target 
+			// e.g.:  {3,1,0} -> {3,2,1}
+			floor_arr[2] = floor_arr[1];
+			floor_arr[1] = floor;
+			is_target_set = 0;
+			return;
+		} 
+	}
+	
+	//for floor 1 and 3
+	for( i = 0; i < 3; i++){
+		if(floor_arr[i] == 0){
+			floor_arr[i] = floor;
+			is_target_set = 0;
+			return;
+		}
+	} 
+	
+	
+}
+
+void removeFloor (int Floor) {
+
+}
 
 static void plannerTask(void *params) {
-	static portTickType xLastWakeTime;
 	xLastWakeTime = xTaskGetTickCount();
 	for(;;){
-		// ...
-		//printf("Planner Task started \n");
 		xQueueReceive(pinEventQueue, &event, portMAX_DELAY);
 		printf("Event recieved: %s \n", event_str(event));
-		
-		switch( event ){
-			case(TO_FLOOR_2): 
-				vTaskDelay(1000/portTICK_RATE_MS);
-				setCarTargetPosition(FLOOR_2_POS); break; 
-		
+		switch( event ) {
+			case(STOP_PRESSED) : stop_state = 1; setCarMotorStopped(1); break;
+			case(STOP_RELEASED): stop_state = 0; break;
+			case(TO_FLOOR_1): addFloor(0); break;
+			case(TO_FLOOR_2): addFloor(1); break;
+			case(TO_FLOOR_3): addFloor(2); break;
+			// need to add other events
 		}
-		// Make decision from event 
-		// TO_FLOOR_1
-		// TO_FLOOR_2
-		// TO_FLOOR_3
-		// ARRIVED_AT_FLOOR
-		// LEFT_FLOOR
-		// DOORS_CLOSED
-		// DOORS_OPENING
-		// STOP_PRESSED
-		// STOP_RELEASED
-
-    
+		
+		if(stop_state != 1 && is_target_set != 1){
+			switch(floor_arr[0]) {
+				case(1) : 
+					vTaskDelay(1000/portTICK_RATE_MS);
+					setCarTargetPosition(TO_FLOOR_1);
+					is_target_set = 1;
+					break;
+				case(2) : 
+					vTaskDelay(1000/portTICK_RATE_MS);
+					setCarTargetPosition(TO_FLOOR_2);
+					is_target_set = 1;
+					break;
+				case(3) : 
+					vTaskDelay(1000/portTICK_RATE_MS);
+					setCarTargetPosition(TO_FLOOR_3);
+					is_target_set = 1;
+					break;
+			}		
+		}
 		vTaskDelayUntil(&xLastWakeTime, PLANNER_POLL);
 	}
 
